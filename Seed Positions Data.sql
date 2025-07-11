b2d17b9a64236f9f43825710843b1ec304f0efd9
Mohamed-Amin <166614121+MoohamedAmin-wo@users.noreply.github.com>
2025-07-11 17:06:42 +0300

    Add files via upload

---
+++ Seed Positions Data.sql
@@ -0,0 +1,14 @@
+﻿SET IDENTITY_INSERT [Support].[Positions] ON
+INSERT INTO [Support].[Positions] ([Id], [PositionName], [PositionAPPR]) VALUES (1, 'Striker', 'SS')
+INSERT INTO [Support].[Positions] ([Id], [PositionName], [PositionAPPR]) VALUES (2, 'RightWing', 'RW')
+INSERT INTO [Support].[Positions] ([Id], [PositionName], [PositionAPPR]) VALUES (3, 'LeftWing', 'LW')
+INSERT INTO [Support].[Positions] ([Id], [PositionName], [PositionAPPR]) VALUES (4, 'RightCentreBack', 'RCB')
+INSERT INTO [Support].[Positions] ([Id], [PositionName], [PositionAPPR]) VALUES (5, 'LeftCentreBack', 'LCB')
+INSERT INTO [Support].[Positions] ([Id], [PositionName], [PositionAPPR]) VALUES (6, 'RightWingBack', 'RWB')
+INSERT INTO [Support].[Positions] ([Id], [PositionName], [PositionAPPR]) VALUES (7, 'LeftWingBack', 'LWB')
+INSERT INTO [Support].[Positions] ([Id], [PositionName], [PositionAPPR]) VALUES (8, 'CentralDefence', 'CD')
+INSERT INTO [Support].[Positions] ([Id], [PositionName], [PositionAPPR]) VALUES (9, 'CentreMidfielder', 'CM')
+INSERT INTO [Support].[Positions] ([Id], [PositionName], [PositionAPPR]) VALUES (10, 'RightMidfielder', 'RM')
+INSERT INTO [Support].[Positions] ([Id], [PositionName], [PositionAPPR]) VALUES (11, 'LeftMidfielder', 'LM')
+INSERT INTO [Support].[Positions] ([Id], [PositionName], [PositionAPPR]) VALUES (12, 'GoalKeeper', 'GK')
+SET IDENTITY_INSERT [Support].[Positions] OFF
